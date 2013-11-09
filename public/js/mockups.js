446cb1c67240d4141347f7fd5d562219022a7083
williamnewton <williamnewbton@gmail.com>
2013-11-09 01:11:44 -0800

    Styles

--- public/js/mockups.js
+++ public/js/mockups.js
@@ -5,6 +5,48 @@ var hitLoop = function () {
       window.setTimeout(hitLoop, 1000);
   };
 
+// hitLoop();
 
 
+var goal = function () {
+  $(".goal").toggle();
+  $(".goal h1").addclass('visible');
+};
+
+var goalReset = function () {
+		$(".goal").addClass("explode");
+		window.setTimeout(function(){
+			$('.goal').remove();
+		}, 300);
+	};
+
+$(document).on('click', ".goal",
+	function () {
+		$(".goal").addClass("explode");
+		window.setTimeout(function(){
+			$('.goal').remove();
+		}, 300);
+	});
 });
+
+
+
+/*
+
+
+var goalReset = function () {
+	$(".goal").addClass('explode');
+	window.setTimeout(function(){
+		$('.goal').remove();
+	}, 300);
+};
+
+var goal = function () {
+  $(".goal").toggle();
+  $(".goal").addClass('visible');
+  $(".goal h1").addClass('visible');
+  window.setTimeout(goalReset(), 3000);
+};
+
+
+*/
