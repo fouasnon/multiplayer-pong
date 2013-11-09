1f3abee57d9a0dddb1b7fb0ceafd760641a70735
williamnewton <williamnewbton@gmail.com>
2013-11-09 04:37:14 -0800

    Style updates, going-down class and going-right class

--- public/js/mockups.js
+++ public/js/mockups.js
@@ -2,7 +2,7 @@ $(document).ready(function () {
 
 	var hitLoop = function () {
       $(".ball").toggleClass("hit");
-      window.setTimeout(hitLoop, 1000);
+      window.setTimeout(hitLoop, 2000);
   	};
 
 // hitLoop();
