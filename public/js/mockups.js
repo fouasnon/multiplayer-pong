a837603cac9f047dd93c44eedd7de26f443b29ba
williamnewton <williamnewbton@gmail.com>
2013-11-09 18:05:00 -0800

    team display

--- public/js/mockups.js
+++ public/js/mockups.js
@@ -3,8 +3,9 @@ $(document).ready(function () {
 	var hitLoop = function () {
       $(".ball").toggleClass("going-right");
       $(".ball").toggleClass("going-down");
-      window.setTimeout(hitLoop, 2000);
+      window.setTimeout(hitLoop, 3000);
   	};
+
 
 });
 
